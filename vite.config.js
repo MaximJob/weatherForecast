bf7f415a1048f86341a55638e8e6ec7cdfdb07ec
Максим Алейников <m.aleynikov@vpene.ru>
2022-04-13 12:55:50 +0300

    Edits

--- vite.config.js
+++ vite.config.js
@@ -14,25 +14,7 @@ export default defineConfig({
   plugins: [
     createVuePlugin(),
     VitePWA({
-      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
-      manifest: {
-        name: "Прогноз погоды",
-        short_name: "Прогноз погоды",
-        description: "Прогноз погоды",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"]
     })
   ]
 });
