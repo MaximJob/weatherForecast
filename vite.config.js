b10388e7774c16b3e1f304d057cc87817a066064
Максим Алейников <m.aleynikov@vpene.ru>
2022-04-13 13:46:19 +0300

    Edits

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   plugins: [
     createVuePlugin(),
     VitePWA({
-      srcDir: "src",
+      srcDir: "public",
       filename: "sw.js",
       registerType: "autoUpdate",
       strategies: "injectManifest",
@@ -26,17 +26,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "android-chrome-192x192.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "android-chrome-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png"
           },
           {
-            src: "android-chrome-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
