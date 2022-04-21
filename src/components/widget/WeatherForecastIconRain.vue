b9865a5d91a3448dd136e0d80306b1e7c7d96adf
Максим Алейников <m.aleynikov@vpene.ru>
2022-04-21 16:17:30 +0300

    edits

---
+++ src/components/widget/WeatherForecastIconRain.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "WeatherForecastIconRain"
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/widget/WeatherForecastIconSnow.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherForecastIconSnow"
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
