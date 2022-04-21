0bc451f95f25fe95f0b27b36301595409ccdc056
Максим Алейников <m.aleynikov@vpene.ru>
2022-04-21 16:13:50 +0300

    Edits

--- src/components/widget/WeatherForecastIconSun.vue
+++ src/components/widget/WeatherForecastIconSun.vue
@@ -20,7 +20,7 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  animation: 5s sun linear infinite;
+  animation: 10s sun linear infinite;
   overflow: hidden;
 
   .bg {
@@ -29,40 +29,105 @@ export default {
     position: absolute;
     background-color: #ff9800;
     clip-path: polygon(
-        0% 0%,
-        25% 25%,
-        50% 0%,
-        75% 25%,
-        100% 0%,
-        75% 25%,
-        100% 50%,
-        75% 75%,
-        100% 100%,
-        100% 100%,
-        100% 100%,
-        100% 100%,
-        0% 100%,
-        0% 100%,
-        0% 100%,
-        0% 100%
+        15% 15%,
+        //
+        37% 20%,
+        50% 5%,
+        63% 20%,
+        //
+        85% 15%,
+        //
+        80% 37%,
+        95% 50%,
+        80% 63%,
+        //
+        85% 85%,
+        //
+        63% 80%,
+        50% 95%,
+        37% 80%,
+        //
+        15% 85%,
+        //
+        20% 63%,
+        5% 50%,
+        20% 37%
     );
+    animation: 0.5s bg linear infinite alternate;
+
+    @keyframes bg {
+      from {
+        clip-path: polygon(
+            10% 10%,
+            //
+            37% 20%,
+            50% 0,
+            63% 20%,
+            //
+            90% 10%,
+            //
+            80% 37%,
+            100% 50%,
+            80% 63%,
+            //
+            90% 90%,
+            //
+            63% 80%,
+            50% 100%,
+            37% 80%,
+            //
+            10% 90%,
+            //
+            20% 63%,
+            0 50%,
+            20% 37%
+        );
+      }
+      to {
+        clip-path: polygon(
+            15% 15%,
+            //
+            37% 20%,
+            50% 5%,
+            63% 20%,
+            //
+            85% 15%,
+            //
+            80% 37%,
+            95% 50%,
+            80% 63%,
+            //
+            85% 85%,
+            //
+            63% 80%,
+            50% 95%,
+            37% 80%,
+            //
+            15% 85%,
+            //
+            20% 63%,
+            5% 50%,
+            20% 37%
+        );
+      }
+    }
   }
 
   .circle {
-    width: 80%;
-    height: 80%;
+    width: 55%;
+    height: 55%;
     position: absolute;
-    //background-color: #ffeb3b;
+    background-color: #ffeb3b;
     border-radius: 50%;
     overflow: hidden;
-    animation: 1s circle linear infinite alternate;
+    animation: 0.5s circle linear infinite alternate;
 
     @keyframes circle {
       from {
         transform: scale(1);
       }
       to {
-        transform: scale(0.9);
+        transform: scale(0.95);
       }
     }
   }
@@ -72,8 +137,8 @@ export default {
       transform: rotate(0);
     }
     to {
-      transform: rotate(0);
-      //transform: rotate(360deg);
+      //transform: rotate(0);
+      transform: rotate(360deg);
     }
   }
 }
